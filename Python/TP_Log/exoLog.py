3da5dfe98bc1b40bc053f1ee18fe91abbf662143
nazimbo <113207575+nazimbo@users.noreply.github.com>
2022-10-13 14:23:00 +0200

    tpLog

---
+++ Python/TP_Log/exoLog.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+mydict = dict()
+
+# ouverture fichier mode lecture
+f = open("/Users/astro/Desktop/ESGI/Python/TP_Log/auth.V1.log", "r")
+lines = f.readlines()
+
+# parcours fichier
+for line in lines:
+    mydict[line] = line.split()[10]
+
+count = Counter(mydict.values())
+print(count)
+
+sortedCount = dict(
+    sorted(count.items(), key=lambda item: item[1], reverse=True))
+
+for index, key in enumerate(sortedCount):
+    if index < 10:
+        print(f"#{index + 1} {key}: ({sortedCount[key]} fois)")
+
+
---
+++ Python/TP_Log/serie1.js
@@ -0,0 +1,22 @@
+Highcharts.chart('serie1',
+{
+        chart: {
+            type: 'pie'
+        },
+		title: {
+			text: 'Part de marché des navigateurs'
+		},
+		subtitle: {
+			text: '... dans un monde idéal'
+		},
+        series: [{
+            data: [
+					['Firefox', 54],
+					['Chrome', 17],
+					['Edge', 16],
+					['Opera', 4],
+					['Safari', 3],
+					['Autres', 6],
+        ]
+        }]
+    });
